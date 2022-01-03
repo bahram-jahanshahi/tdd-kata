230a6415c7b8d79ecf56ac15b5c6916f52686ef1
Bahram Jahanshahi <bahram.jahanshahi@gmail.com>
2022-01-03 18:01:18 +0100

    Initial Country Rest Api Kata

---
+++ country-rest-api/src/test/java/se/bahram/tdd/kata/countryrestapi/CountryRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.bahram.tdd.kata.countryrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
