0983b4725292fd3f92cf9e742eb30aee8625853f
Bahram Jahanshahi <bahram.jahanshahi@gmail.com>
2022-01-05 20:02:04 +0100

    Refactor code and add country service by TDD #4

---
+++ country-rest-api/src/main/java/se/bahram/tdd/kata/countryrestapi/services/CountryService.java
@@ -0,0 +1,18 @@
+package se.bahram.tdd.kata.countryrestapi.services;
+
+import se.bahram.tdd.kata.countryrestapi.model.Country;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+public class CountryService {
+    public Optional<Country> findById(int i) {
+        return Optional.empty();
+    }
+
+    public List<String> findAll() {
+        return Arrays.asList();
+    }
+}
---
+++ country-rest-api/src/test/java/se/bahram/tdd/kata/countryrestapi/services/CountryServiceTest.java
@@ -0,0 +1,22 @@
+package se.bahram.tdd.kata.countryrestapi.services;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+public class CountryServiceTest {
+
+
+    @Test
+    void when_find_by_id_it_should_return_empty() {
+        CountryService countryService = new CountryService();
+        assertThat(countryService.findById(1)).isEqualTo(Optional.empty());
+    }
+
+    @Test
+    void when_find_all_it_should_return_empty_list() {
+        CountryService countryService = new CountryService();
+        assertThat(countryService.findAll().size()).isEqualTo(0);
+    }
+}
